1ac3914eabbc2c9dda4d3ce0ef9fca0db3d60ce1
Sorabh <ssorabh.ssharma@gmail.com>
2017-07-20 19:02:25 +0530

    dynamic and paypal

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -10,9 +10,18 @@ angular.module('templateStore.templates', ['ngRoute'])
 		controller : 'TemplateDetailsCtrl'
 	});
 }])
-.controller('TemplateCtrl',['$scope', function($scope){
-	console.log('TemplateCtrl runs', $scope);
+.controller('TemplateCtrl',['$scope', '$http', function($scope, $http){
+	$http.get('json/templates.json').success(function(data){
+		$scope.templates = data;
+	});
 }])
-.controller('TemplateDetailsCtrl',['$scope', function($scope){
-	console.log('TemplateCtrl runs', $scope);
+.controller('TemplateDetailsCtrl',['$scope', '$routeParams', '$http', '$filter', function($scope, $routeParams, $http, $filter){
+	var templateId = $routeParams.templateId;
+	$http.get('json/templates.json').success(function(data){
+		$scope.template = $filter('filter')(data,function(d){
+			return d.id==templateId;
+		})[0];
+		console.log($scope.template);
+		$scope.mainImage = $scope.template.images[0].name;
+	});
 }]);
